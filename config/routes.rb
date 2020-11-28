8d066c70447b604d991bb09e3617316f42194d80
alexander <alexander.haumer@me.com>
2020-11-28 02:52:22 +0000

    upgraded gems and migrated

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [ :show ]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dashboards, only: [ :edit, :update ]
+  resources :sections, only: [ :edit, :update ]
+
+  get "d/:user", to: "dashboards#show"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_191846) do
+ActiveRecord::Schema.define(version: 2020_11_28_024200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dashboards", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dashboards_on_user_id"
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.bigint "dashboard_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dashboard_id"], name: "index_sections_on_dashboard_id"
+  end
+
+  create_table "traces", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +46,6 @@ ActiveRecord::Schema.define(version: 2020_04_24_191846) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dashboards", "users"
+  add_foreign_key "sections", "dashboards"
 end
