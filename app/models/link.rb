1a609287df1ad612c4c0bef0ed115f111bce03b9
alexander <alexander.haumer@me.com>
2020-12-05 22:26:54 +0000

    callback to get link data

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,18 @@
 class Link < ApplicationRecord
   after_create :get_meta_tags
+  after_create :get_favicon
 
   def get_meta_tags
     page = MetaInspector.new(url)
+
+    self.title = page.title
+    self.image = page.meta_tags['property']['og:image'].first
+    save
+  end
+
+  def get_favicon
+    host = URI(url).host
+    self.favicon = "#{host}/favicon.ico"
+    save
   end
 end
