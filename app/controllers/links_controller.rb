4629d8f74010c0d899b4af5ee9ef9da4c6d4781d
alexander <alexander.haumer@me.com>
2021-01-03 20:55:29 +0000

    controller links create action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,17 @@
 class LinksController < ApplicationController
+  def create
+    @link = Link.new(link_params)
+    if @link.save!
+      redirect_back(fallback_location: root_path)
+      flash[:notice] = "Success!"
+    else
+      flash[:notice] = "Something went wrong!"
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
 end
